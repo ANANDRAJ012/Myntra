38c96f6109ae67c8e529dc0c5232d5bd0efac3e3
ANAND RAJ <anandraj93054@gmail.com>
2024-08-24 20:07:07 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-var countDate = new Date("AUG 12, 2023 21:30:00").getTime();
+var countDate = new Date("AUG 12, 2025 21:30:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDate - now;
